14d7eb7a308d95fd68065cfd4359bc7faeb3bcc4
staffuser <119116574+staffuser@users.noreply.github.com>
2023-10-30 17:28:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function locationreload() {
+    location.reload();
+    
+}
+
+window.dataLayer = window.dataLayer || [];
+    
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-EPYS5J2XZM');
